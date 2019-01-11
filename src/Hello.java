e9e61baa1e56b762d13d557a3ee8425f4f77313d
En Zhou <zhouen.nathan@gmail.com>
2019-01-11 22:47:47 +0200

    Created repo

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Java!");
+
+        int myFirstNumber = 5;
+        int mySecondNumber = 11;
+        int myThridNumber = 34;
+        int myTotal = myFirstNumber + mySecondNumber + myThridNumber;
+        System.out.println(myTotal);
+    }
+}
